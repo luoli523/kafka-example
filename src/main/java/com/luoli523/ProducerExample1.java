d9f27be763840d53484922810b0192a7451d34e9
luoli <luoli523@gmail.com>
2020-04-24 23:49:00 +0800

    init the kafka example project

---
+++ src/main/java/com/luoli523/ProducerExample1.java
@@ -0,0 +1,30 @@
+package com.luoli523;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class ProducerExample1 {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092,localhost:9093,localhost:9094");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+        for (int i = 0; i < 100; i++) {
+            producer.send(new ProducerRecord<String, String>("exam-topic", Integer.toString(i), Integer.toString(i)));
+            System.out.println("sent " + i);
+        }
+
+        producer.close();
+    }
+
+}
